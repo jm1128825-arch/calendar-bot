7a391c085ce508f49cdadadf12b5431e7a71fbd0
jm1128825-arch <jm1128825@gmail.com>
2026-07-14 14:48:12 -0700

    Update calendar_utils.py

--- calendar_utils.py
+++ calendar_utils.py
@@ -2,14 +2,29 @@ import calendar
 from datetime import datetime
 
 
-def get_current_month():
-    now = datetime.now()
-
-    year = now.year
-    month = now.month
+def get_month(year=None, month=None):
+    if year is None or month is None:
+        now = datetime.now()
+        year = now.year
+        month = now.month
 
     cal = calendar.monthcalendar(year, month)
-
     month_name = calendar.month_name[month]
 
     return year, month, month_name, cal
+
+
+def previous_month(year, month):
+    month -= 1
+    if month == 0:
+        month = 12
+        year -= 1
+    return year, month
+
+
+def next_month(year, month):
+    month += 1
+    if month == 13:
+        month = 1
+        year += 1
+    return year, month
