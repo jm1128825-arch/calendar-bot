f7d2e8ede7e1ea9fd59f75497bc29c1ab4de2e83
jm1128825-arch <jm1128825@gmail.com>
2026-07-14 14:28:44 -0700

    Update calendar_utils.py

--- calendar_utils.py
+++ calendar_utils.py
@@ -1,1 +1,15 @@
-# Calendar functions will go here.
+import calendar
+from datetime import datetime
+
+
+def get_current_month():
+    now = datetime.now()
+
+    year = now.year
+    month = now.month
+
+    cal = calendar.monthcalendar(year, month)
+
+    month_name = calendar.month_name[month]
+
+    return year, month, month_name, cal
